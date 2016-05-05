5e4893e87167b01a23207bb00b89d776acf18443
jvbsl <jvbsl@novacyb.de>
2016-05-05 16:47:17 +0200

    Forgot files

---
+++ ContentPipeline/Pipeline/BuildMessageEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace engenious.Content.Pipeline
+{
+    public class BuildMessageEventArgs
+    {
+        public enum BuildMessageType
+        {
+            None,
+            Warning,
+            Error,
+            Information
+        }
+        public BuildMessageEventArgs(string filename,string message,BuildMessageType messageType)
+        {
+            FileName = filename;
+            Message = message;
+            MessageType = messageType;
+        }
+        public string FileName { get; private set; }
+        public string Message { get; private set; }
+        public BuildMessageType MessageType { get; private set; }
+    }
+}
---
+++ ContentPipeline/Pipeline/IContentContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace engenious.Content.Pipeline
+{
+    public delegate void BuildMessageDel(object sender,BuildMessageEventArgs e);
+    public interface IContentContext : IDisposable
+    {
+        List<string> Dependencies { get; }
+
+        void RaiseBuildMessage(string filename,string message, BuildMessageEventArgs.BuildMessageType messageType);
+
+        event BuildMessageDel BuildMessage;
+    }
+}
