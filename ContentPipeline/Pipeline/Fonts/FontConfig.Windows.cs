134b4c2ede5234b9ba596440d6dab39ee5efbc4a
Fabian Nawratil <fabian@nawratil.me>
2016-10-02 15:03:05 +0200

    Successfully getting the font file Name(to load with SharpFont)

--- ContentPipeline/Pipeline/Fonts/FontConfig.Windows.cs
+++ ContentPipeline/Pipeline/Fonts/FontConfig.Windows.cs
@@ -5,297 +5,232 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using Microsoft.Win32;
 
 
 namespace engenious.Pipeline
 {
     public class FontConfigWindows : FontConfig
 	{
+        private Dictionary<string,string> fontFileMap = new Dictionary<string, string>();
+
+        private bool findFondFile(ref string fileName)
+        {
+            if (Path.GetExtension(fileName) != ".ttf") return false;
+            string file = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), fileName);
+            if (File.Exists(file))
+            {
+                fileName = file;
+                return true;
+            }
+            if (File.Exists(fileName)) return true;
+            return false;
+        }
 		public FontConfigWindows()
 		{
+		    var fontKey = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts");
+		    if (fontKey == null)
+		        return;
+		    foreach (string fontName in fontKey.GetValueNames())
+		    {
+		        var value = fontKey.GetValue(fontName, null);
+		        if (value == null) continue;
+		        var file = value.ToString();
+		        if (findFondFile(ref file))
+		        {
+		            string name = fontName;
+		            if (name.EndsWith(" (TrueType)"))
+                        name = name.Substring(0, name.Length - " (TrueType)".Length);//TODO: better solution?
+		            fontFileMap.Add(name, file);
+		        }
+		    }
 		}
 
         #region implemented abstract members of FontConfig
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left, Top, Right, Bottom;
-
-            public RECT(int left, int top, int right, int bottom)
-            {
-                Left = left;
-                Top = top;
-                Right = right;
-                Bottom = bottom;
-            }
-
-            public RECT(System.Drawing.Rectangle r) : this(r.Left, r.Top, r.Right, r.Bottom) { }
-
-            public int X
-            {
-                get { return Left; }
-                set { Right -= (Left - value); Left = value; }
-            }
-
-            public int Y
-            {
-                get { return Top; }
-                set { Bottom -= (Top - value); Top = value; }
-            }
-
-            public int Height
-            {
-                get { return Bottom - Top; }
-                set { Bottom = value + Top; }
-            }
-
-            public int Width
-            {
-                get { return Right - Left; }
-                set { Right = value + Left; }
-            }
-
-            public System.Drawing.Point Location
-            {
-                get { return new System.Drawing.Point(Left, Top); }
-                set { X = value.X; Y = value.Y; }
-            }
-
-            public System.Drawing.Size Size
-            {
-                get { return new System.Drawing.Size(Width, Height); }
-                set { Width = value.Width; Height = value.Height; }
-            }
-
-            public static implicit operator System.Drawing.Rectangle(RECT r)
-            {
-                return new System.Drawing.Rectangle(r.Left, r.Top, r.Width, r.Height);
-            }
-
-            public static implicit operator RECT(System.Drawing.Rectangle r)
-            {
-                return new RECT(r);
-            }
-
-            public static bool operator ==(RECT r1, RECT r2)
-            {
-                return r1.Equals(r2);
-            }
-
-            public static bool operator !=(RECT r1, RECT r2)
-            {
-                return !r1.Equals(r2);
-            }
-
-            public bool Equals(RECT r)
-            {
-                return r.Left == Left && r.Top == Top && r.Right == Right && r.Bottom == Bottom;
-            }
-
-            public override bool Equals(object obj)
-            {
-                if (obj is RECT)
-                    return Equals((RECT)obj);
-                else if (obj is System.Drawing.Rectangle)
-                    return Equals(new RECT((System.Drawing.Rectangle)obj));
-                return false;
-            }
-
-            public override int GetHashCode()
-            {
-                return ((System.Drawing.Rectangle)this).GetHashCode();
-            }
-
-            public override string ToString()
-            {
-                return string.Format(System.Globalization.CultureInfo.CurrentCulture, "{{Left={0},Top={1},Right={2},Bottom={3}}}", Left, Top, Right, Bottom);
-            }
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT
-        {
-            public int X;
-            public int Y;
-
-            public POINT(int x, int y)
-            {
-                this.X = x;
-                this.Y = y;
-            }
-
-            public POINT(System.Drawing.Point pt) : this(pt.X, pt.Y) { }
-
-            public static implicit operator System.Drawing.Point(POINT p)
-            {
-                return new System.Drawing.Point(p.X, p.Y);
-            }
-
-            public static implicit operator POINT(System.Drawing.Point p)
-            {
-                return new POINT(p.X, p.Y);
-            }
-        }
-        [Serializable, StructLayout(LayoutKind.Sequential, CharSet=CharSet.Auto)]
-        private struct TEXTMETRIC
-        {
-            public int tmHeight;
-            public int tmAscent;
-            public int tmDescent;
-            public int tmInternalLeading;
-            public int tmExternalLeading;
-            public int tmAveCharWidth;
-            public int tmMaxCharWidth;
-            public int tmWeight;
-            public int tmOverhang;
-            public int tmDigitizedAspectX;
-            public int tmDigitizedAspectY;
-            public byte tmFirstChar;    // for compatibility issues it must be byte instead of char (see the comment for further details above)
-            public byte tmLastChar;    // for compatibility issues it must be byte instead of char (see the comment for further details above)
-            public byte tmDefaultChar;    // for compatibility issues it must be byte instead of char (see the comment for further details above)
-            public byte tmBreakChar;    // for compatibility issues it must be byte instead of char (see the comment for further details above)
-            public byte tmItalic;
-            public byte tmUnderlined;
-            public byte tmStruckOut;
-            public byte tmPitchAndFamily;
-            public byte tmCharSet;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct PANOSE
-        {
-            public byte bFamilyType;
-            public byte bSerifStyle;
-            public byte bWeight;
-            public byte bProportion;
-            public byte bContrast;
-            public byte bStrokeVariation;
-            public byte bArmStyle;
-            public byte bLetterform;
-            public byte bMidline;
-            public byte bXHeight;
-        }
-        [StructLayout(LayoutKind.Sequential)]
-        private struct OUTLINETEXTMETRIC
-        {
-            public uint     otmSize;
-            public TEXTMETRIC otmTextMetrics;
-            public byte     otmFiller;
-            public PANOSE      otmPanoseNumber;
-            public uint     otmfsSelection;
-            public uint     otmfsType;
-            public int    otmsCharSlopeRise;
-            public int     otmsCharSlopeRun;
-            public int     otmItalicAngle;
-            public uint     otmEMSquare;
-            public int     otmAscent;
-            public int     otmDescent;
-            public uint     otmLineGap;
-            public uint     otmsCapEmHeight;
-            public uint     otmsXHeight;
-            public RECT     otmrcFontBox;
-            public int        otmMacAscent;
-            public int        otmMacDescent;
-            public uint        otmMacLineGap;
-            public uint        otmusMinimumPPEM;
-            public POINT       otmptSubscriptSize;
-            public POINT       otmptSubscriptOffset;
-            public POINT       otmptSuperscriptSize;
-            public POINT       otmptSuperscriptOffset;
-            public uint        otmsStrikeoutSize;
-            public int        otmsStrikeoutPosition;
-            public int        otmsUnderscoreSize;
-            public int        otmsUnderscorePosition;
-            public IntPtr      otmpFamilyName;
-            public IntPtr      otmpFaceName;
-            public IntPtr      otmpStyleName;
-            public IntPtr      otmpFullName;
-        }
-        // if we specify CharSet.Auto instead of CharSet.Ansi, then the string will be unreadable
-        [StructLayout(LayoutKind.Sequential, CharSet=CharSet.Auto)]
-        public class LOGFONT
-        {
-            public int lfHeight = 0;
-            public int lfWidth = 0;
-            public int lfEscapement = 0;
-            public int lfOrientation = 0;
-            public int lfWeight = 0;
-            public byte lfItalic = 0;
-            public byte lfUnderline = 0;
-            public byte lfStrikeOut = 0;
-            public byte lfCharSet = 0;
-            public byte lfOutPrecision = 0;
-            public byte lfClipPrecision = 0;
-            public byte lfQuality = 0;
-            public byte lfPitchAndFamily = 0;
-            [MarshalAs(UnmanagedType.ByValTStr, SizeConst=32)]
-            public string lfFaceName = string.Empty;
-        }
-        [DllImport("user32.dll")]
-        static extern IntPtr GetDC(IntPtr hWnd);
-        [DllImport("user32.dll")]
-        static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
-        [DllImport("gdi32.dll")]
-        static extern uint GetOutlineTextMetrics(IntPtr hdc, uint cbData, IntPtr ptrZero);
-        [DllImport("gdi32.dll", EntryPoint = "SelectObject")]
-        public static extern IntPtr SelectObject([In] IntPtr hdc, [In] IntPtr hgdiobj);
-
-        private static void GetOutlineMetrics(IntPtr hdc)
-        {
-            uint cbBuffer = GetOutlineTextMetrics(hdc, 0, IntPtr.Zero);
-            if (cbBuffer == 0)
-                return;
-            IntPtr buffer = Marshal.AllocHGlobal((int)cbBuffer);
-            try
-            {
-                if (GetOutlineTextMetrics(hdc, cbBuffer, buffer) != 0)
-                {
-                    OUTLINETEXTMETRIC otm = new OUTLINETEXTMETRIC();
-                    otm = (OUTLINETEXTMETRIC)Marshal.PtrToStructure(buffer, typeof(OUTLINETEXTMETRIC));
-                    string otmpFamilyName = Marshal.PtrToStringAnsi(buffer + otm.otmpFamilyName.ToInt32());
-                    string otmpFaceName = Marshal.PtrToStringAnsi(buffer + otm.otmpFaceName.ToInt32());
-                    string otmpStyleName = Marshal.PtrToStringAnsi(buffer + otm.otmpStyleName.ToInt32());
-                    string otmpFullName = Marshal.PtrToStringAnsi(buffer + otm.otmpFullName.ToInt32());
-                }
-            }
-            finally
-            {
-                Marshal.FreeHGlobal(buffer);
-            }
-        }
-
-        private IEnumerable<uint> checksums(System.IO.Stream stream)
-        {
-            byte[] buffer = new byte[6];
-            stream.Read(buffer,0,buffer.Length);
-            ushort numTables = BitConverter.ToUInt16(buffer,4);
-            buffer = new byte[16*numTables];
-            stream.Read(buffer,0,buffer.Length);
-            for (int i=0;i<numTables;i++)
-            {
-                yield return BitConverter.ToUInt32(buffer,i*16+4);
-            }
-            yield break;
-        }
-        [DllImport("gdi32.dll")]
-        private static extern uint GetFontData(IntPtr hdc, uint dwTable, uint dwOffset, [Out] byte[] lpvBuffer, uint cbData);
+       
 
         public override string GetFontFile(string fontName, int fontSize, System.Drawing.FontStyle style)
         {
+            System.Drawing.Font fnt = new System.Drawing.Font(fontName, fontSize, style, System.Drawing.GraphicsUnit.Point);
+
+            var names = GetFontNames(fnt);
+            string fileName=null;
+            foreach (var name in names)
+            {
+                if (fontFileMap.TryGetValue(name.Name, out fileName))
+                    break;
+            }
+            return fileName;
+        }
+
+        private List<NameRecord> GetFontNames(System.Drawing.Font font)
+        {
+            return GetFontNames(font.ToHfont());
+        }
+        private List<NameRecord> GetFontNames(IntPtr hFont)
+        {
+            var fontNames = new List<NameRecord>();
             IntPtr dc = GetDC(IntPtr.Zero);
 
-            System.Drawing.Font fnt = new System.Drawing.Font("Arial", fontSize, style, System.Drawing.GraphicsUnit.Point);
-            IntPtr hFont = fnt.ToHfont();
             SelectObject(dc, hFont);
-            //GetOutlineMetrics(dc);
 
-            //ReleaseDC(IntPtr.Zero, dc);
+            using (BinaryReader br = new BinaryReader(new MemoryStream(LoadFontMetricsNameTable(dc))))
+            {
+                // Read selector (always = 0) to advance reader position by 2 bytes
 
-            var name = FontNameExtractor.GetFullFontName(dc);
-            Console.WriteLine(name);
-            return @"C:\Windows\Fonts\arial.ttf";
+                ushort selector = ToLittleEndian(br.ReadUInt16());
+
+                // Get number of records and offset byte value, from where font descriptions start
+
+                ushort records = ToLittleEndian(br.ReadUInt16());
+                ushort offset = ToLittleEndian(br.ReadUInt16());
+
+                // Get the correct name record
+                for (ushort i = 0; i < records; i++)
+                {
+                    NameRecord nameRecord = new NameRecord(br);
+                    if (nameRecord.IsWindowsUnicodeFullFontName)
+                        fontNames.Add(nameRecord);
+                }
+                foreach(var record in fontNames)
+                    record.ReadName(br,offset);
+
+
+                br.Close();
+            }
+
+            ReleaseDC(IntPtr.Zero, dc);
+
+            return fontNames;
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetDC(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        private static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        [DllImport("gdi32.dll", EntryPoint = "SelectObject")]
+        private static extern IntPtr SelectObject([In] IntPtr hdc, [In] IntPtr hgdiobj);
+        [DllImport("gdi32.dll")]
+        private static extern uint GetFontData(IntPtr hdc, uint dwTable, uint dwOffset, [Out] byte[] lpvBuffer, uint cbData);
+
+        private const uint NameTableKey = 0x656D616E;
+        /// <summary>
+        /// Loads the font metrics name table as raw bytes from the font in the <paramref name="dc"/>.
+        /// </summary>
+        /// <param name="dc">The DC hosting the Font.</param>
+        /// <returns>The metrics table as raw bytes.</returns>
+        private static byte[] LoadFontMetricsNameTable(IntPtr dc)
+        {
+            byte[] fontData = null;
+
+            // First determine the amount of bytes in the font metrics name table
+
+            uint byteCount = GetFontData(dc, NameTableKey, 0, null, 0);
+            // Now init the byte array and load the data by calling GetFontData once again
+
+            fontData = new byte[byteCount];
+            GetFontData(dc, NameTableKey, 0, fontData, byteCount);
+
+            return fontData;
+        }
+        /// <summary>
+        /// Helper function to convert any <see cref="ushort"/> value in font metrics name table to little endian,
+        ///  because all stored in big endian.
+        /// </summary>
+        /// <param name="value">The value to be converted into little endian byte order.</param>
+        /// <returns>The corresponding <see cref="ushort"/> value in little endian byte order.</returns>
+        private static ushort ToLittleEndian(ushort value)
+        {
+            if (BitConverter.IsLittleEndian)
+            {
+                return (ushort) (value << 8 | value >> 8);
+            }
+
+            return value;
+        }
+
+
+        #endregion
+
+        #region TTF NameRecord Class
+
+        /// <summary>
+        /// Encapsulates a name record as specified in True Type font specification for the font metrics name table
+        /// http://www.microsoft.com/typography/otspec/name.htm
+        /// </summary>
+        private class NameRecord
+        {
+
+            private readonly ushort platformId;
+            private readonly ushort encodingId;
+            private readonly ushort nameId;
+            private ushort NameLength;
+            private ushort ByteOffset;
+
+            public ushort LanguageId { get; private set; }
+            public string Name { get; private set; }
+
+
+
+
+            /// <summary>
+            /// Gets a value indicating whether this <see cref="NameRecord"/> represents a Windows Unicode full font name.
+            /// </summary>
+            /// <value>
+            ///     <c>true</c> if this <see cref="NameRecord"/> represents a Windows Unicode full font name; otherwise, <c>false</c>.
+            /// </value>
+            internal bool IsWindowsUnicodeFullFontName => platformId == 3 && encodingId == 1 && nameId == 4;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="NameRecord"/> class.
+            /// </summary>
+            /// <param name="br">The <see cref="BinaryReader"/> for interpretation of the bytes.</param>
+            internal NameRecord(BinaryReader br)
+            {
+                // Read the unsigned 16-bit integers and convert to little endian
+
+                platformId = ToLittleEndian(br.ReadUInt16());
+                encodingId = ToLittleEndian(br.ReadUInt16());
+
+                // Only read to advance reader position by 2 bytes
+
+                LanguageId = ToLittleEndian(br.ReadUInt16());
+
+                nameId = ToLittleEndian(br.ReadUInt16());
+                NameLength = ToLittleEndian(br.ReadUInt16());
+                ByteOffset = ToLittleEndian(br.ReadUInt16());
+            }
+
+            internal void ReadName(BinaryReader br, int recordOffset)
+            {
+                // Search the start position of the font name
+
+                int totalOffset = recordOffset + ByteOffset;
+                br.BaseStream.Seek(totalOffset, SeekOrigin.Begin);
+
+                // Now read the amount of bytes specified in the name record
+                // and convert to a string
+
+                byte[] nameBytes = br.ReadBytes(NameLength);
+                Name = Encoding.GetEncoding(1201).GetString((nameBytes));
+            }
+
+            /// <summary>
+            /// Returns a <see cref="System.String"/> that represents this instance.
+            /// </summary>
+            /// <returns>
+            /// A <see cref="System.String"/> that represents this instance.
+            /// </returns>
+            public override string ToString()
+            {
+                return
+                    $"Name = {Name ?? "{Not Available}"};NameRecord - Key; Platform ID = {platformId.ToString()}, Encoding ID = {encodingId.ToString()}, Name ID = {nameId.ToString()}";
+            }
+
         }
 
         #endregion
-	}
+    }
 }
 
