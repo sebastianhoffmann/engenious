7cfe7831045d29d6f37a3d3f7f751649ae94e29d
jvbsl <jvbsl@novacyb.com>
2016-10-14 14:47:45 +0200

    Possible Mac fix for libfontconfig

--- ContentPipeline/Pipeline/Fonts/FontConfig.Mac.cs
+++ ContentPipeline/Pipeline/Fonts/FontConfig.Mac.cs
@@ -5,21 +5,21 @@ namespace engenious.Pipeline
 {
     public class FontConfigMac:FontConfigUnix
     {
-        [DllImport("libfontconfig.dylib.1",EntryPoint="FcInitLoadConfigAndFonts")]
+        [DllImport("libfontconfig.1.dylib",EntryPoint="FcInitLoadConfigAndFonts")]
         private static extern IntPtr FcInitLoadConfigAndFonts_Base();
-        [DllImport("libfontconfig.dylib.1",EntryPoint="FcPatternCreate")]
+        [DllImport("libfontconfig.1.dylib",EntryPoint="FcPatternCreate")]
         private static extern IntPtr FcPatternCreate_Base();
-        [DllImport("libfontconfig.dylib.1",EntryPoint="FcNameParse")]
+        [DllImport("libfontconfig.1.dylib",EntryPoint="FcNameParse")]
         private static extern IntPtr FcNameParse_Base(string name);
-        [DllImport("libfontconfig.dylib.1",EntryPoint="FcConfigSubstitute")]
+        [DllImport("libfontconfig.1.dylib",EntryPoint="FcConfigSubstitute")]
         private static extern bool FcConfigSubstitute_Base(IntPtr config,IntPtr pattern,FcMatchKind matchKind);
-        [DllImport("libfontconfig.dylib.1",EntryPoint="FcDefaultSubstitute")]
+        [DllImport("libfontconfig.1.dylib",EntryPoint="FcDefaultSubstitute")]
         private static extern void FcDefaultSubstitute_Base(IntPtr pattern);
-        [DllImport("libfontconfig.dylib.1",EntryPoint="FcFontMatch")]
+        [DllImport("libfontconfig.1.dylib",EntryPoint="FcFontMatch")]
         private static extern IntPtr FcFontMatch_Base(IntPtr config,IntPtr pattern,out FcResult result);
-        [DllImport("libfontconfig.dylib.1",EntryPoint="FcPatternDestroy")]
+        [DllImport("libfontconfig.1.dylib",EntryPoint="FcPatternDestroy")]
         private static extern void FcPatternDestroy_Base(IntPtr pattern);
-        [DllImport("libfontconfig.dylib.1",EntryPoint="FcPatternGetString")]
+        [DllImport("libfontconfig.1.dylib",EntryPoint="FcPatternGetString")]
         private static extern FcResult FcPatternGetString_Base(IntPtr pattern, string name, int n, out IntPtr resultString);
         public FontConfigMac()
         {
